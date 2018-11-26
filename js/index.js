fca04f44e871df9b41f39e687aa26428eb42ed0a
Ana Harris <vana1990@gmail.com>
2018-11-26 10:56:19 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+//
+// function saveLincoln(interval) {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+//   $('#foreground').fadeOut(interval);
+// }
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
-  }
-  $('#foreground').fadeOut(interval);
-}
+document.querySelector("#save_lincoln").addEventListener("click", function(){
+  document.querySelector('#foreground').style.visibility = 'hidden';
+
+  // for (opacity = 1; opacity > 0; opacity = opacity - 0.1 ) {
+  //   setTimeout(function(){document.querySelector('#foreground').style.opacity = opacity;},100)
+  // }
+
+})
